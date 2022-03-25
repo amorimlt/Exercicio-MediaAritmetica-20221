da8e4c5efb23346cb68d0e4589f14edc6c96b255
amorimlt <julianeamorim.lt@gmail.com>
2022-03-25 20:33:28 -0300

    finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿double n1, n2, n3;
+
+Console.Write("Digite o primeiro número: ");
+n1 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Digite o segundo número: ");
+n2 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Digite o terceiro número: ");
+n3 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.WriteLine("A media é: ");
+
+Console.WriteLine($"{(n1 + n2 + n3) / 3:N1}");
